bee52a35453365fd08009408d9438e58de036a35
Ionut Cirja <ionut.cirja@gmail.com>
2020-02-10 11:04:18 +0000

    create app main component / render routes

--- src/app/__tests__/index.spec.tsx
+++ src/app/__tests__/index.spec.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
 
 import App from '..';
 
 describe('App', () => {
+  const MainRouteComponent = () => (<div>Main route</div>);
+  const SecondaryRouteComponent = () => (<div>Secondary route</div>);
+
+  const routes = [
+    {
+      path: '/',
+      exact: true,
+      component: MainRouteComponent,
+    },
+    {
+      path: '/secondary-route',
+      component: SecondaryRouteComponent,
+    },
+  ];
+
   describe('render', () => {
-    it('should render', () => {
-      const wrapper = shallow(<App />);
-      expect(wrapper).toBeDefined();
+    it('should render the right component when navigating to a route', () => {
+      const history = createMemoryHistory();
+      let wrapper = render(
+        <Router history={history}>
+          <App routes={routes} />
+        </Router>,
+      );
+      expect(wrapper.container.innerHTML).toMatch('Main route');
+
+      history.push('/secondary-route');
+      wrapper = render(
+        <Router history={history}>
+          <App routes={routes} />
+        </Router>,
+      );
+      expect(wrapper.container.innerHTML).toMatch('Secondary route');
     });
   });
 });
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { FC, ComponentType } from 'react';
+import { Switch, Route } from 'react-router';
 
-const App: React.FC<{}> = () => (
-  <div>Hello World!</div>
+type RouteProps = {
+  path: string;
+  exact?: boolean;
+  component: ComponentType<any>;
+}
+
+type Props = {
+  routes: RouteProps[];
+}
+
+const App: FC<Props> = ({ routes }: Props) => (
+  <Switch>
+    {routes.map((route: RouteProps) => (
+      <Route
+        key={`path-${route.path}`}
+        exact={route.exact}
+        path={route.path}
+        component={route.component}
+      />
+    ))}
+  </Switch>
 );
 
 export default App;
